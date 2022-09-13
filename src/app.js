2d16b11e65645d488d87be73609bdb457c1ecfbe
vyvy8801 <vynguyen8801@gmail.com>
2022-09-13 20:47:02 +0700

    Install express and show a welcome message

--- src/app.js
+++ src/app.js
@@ -1,24 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const contactController = require('./controllers/contact.controller');
+// const contactController = require('./controllers/contact.controller');
+// const ApiError = require('./api-error');
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to contact book application.' });
-});
-
-app.route('/api/contacts')
-    .get(contactController.findAll)
-    .post(contactController.create)
-    .delete(contactController.deleteAll);
-
-app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
-
-app.route('/api/contacts/:id')
-    .get(contactController.findOne)
-    .put(contactController.update)
-    .delete(contactController.delete);
 
 app.use(cors());
 app.use(express.json());
@@ -27,5 +12,34 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application.'});
 });
 
+// app.route('/api/contacts')
+//     .get(contactController.findAll)
+//     .post(contactController.create)
+//     .delete(contactController.deleteAll);
+
+// app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+
+// app.route('/api/contacts/:id')
+//     .get(contactController.findOne)
+//     .put(contactController.update)
+//     .delete(contactController.delete);
+
+// // Handle 404 response.
+// app.use((req, res, next) => {
+//     // Handler for unknown route.
+//     // Call next() to pass to the error handling middleware.
+//     return next(new ApiError(404, 'Resource not found'));
+//     });
+// // Define error-handling middleware last, after other app.use() and routes calls.
+// app.use((err, req, res, next) => {
+//     // The centralized error handling middleware.
+//     // In any route handler, calling next(error)
+//     // will pass to this error handling middleware.
+
+//     return res.status(error.statusCode || 500).json({
+//         message: error.message || 'Internal Server Error',
+//     });
+
+// });
 module.exports = app;
 
