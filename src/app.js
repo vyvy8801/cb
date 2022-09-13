6ea294fe3173e962cd6c70a5e63bb437863b772a
vyvy8801 <113319617+vyvy8801@users.noreply.github.com>
2022-09-13 23:14:52 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,17 +12,17 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application.'});
 });
 
-app.route('/api/contacts')
-    .get(contactController.findAll)
-    .post(contactController.create)
-    .delete(contactController.deleteAll);
+// app.route('/api/contacts')
+//     .get(contactController.findAll)
+//     .post(contactController.create)
+//     .delete(contactController.deleteAll);
 
-app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+// app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
 
-app.route('/api/contacts/:id')
-    .get(contactController.findOne)
-    .put(contactController.update)
-    .delete(contactController.delete);
+// app.route('/api/contacts/:id')
+//     .get(contactController.findOne)
+//     .put(contactController.update)
+//     .delete(contactController.delete);
 
 // // Handle 404 response.
 // app.use((req, res, next) => {
