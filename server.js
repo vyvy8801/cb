23c5275b7091e188e9815b1f85a656ffa0ab826b
vyvy8801 <vynguyen8801@gmail.com>
2022-09-13 23:10:50 +0700

    Install express and show a welcome message

--- server.js
+++ server.js
@@ -2,15 +2,8 @@ require('dotenv').config();
 const app = require('./src/app');
 
 //start server
-const PORT = 3000;
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
-
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
